72c0296f8440a063301f0a8836d860a1d9289244
DESKTOP-NUOPUIF\ITPS <dbslzhs20@naver.com>
2021-03-15 17:53:12 +0900

    첫째날

--- src/chapter1/Ex1.java
+++ src/chapter1/Ex1.java
@@ -8,14 +8,20 @@ public class Ex1 {
 		// 주석 : 메모, 개발자가 참고해야할 사항
 		// 무언가를 출력하기 위한 기능
 		// () 안에다가 출력 대상을 지정		
-		System.out.println("Hello World~!");	// Hello WOrld~! 문자열 출력
-		System.out.println("주석 연습 중...");		// 주석 연습 중... 출력
-		System.out.println("출력1");
-		System.out.println("출력2");
-		
+//		System.out.println("Hello World~!");	// Hello WOrld~! 문자열 출력
+//		System.out.println("주석 연습 중...");		// 주석 연습 중... 출력
+//		System.out.println("출력1");
+//		System.out.println("출력2");
+//		
 		//		주석 단축키 ctrl + shift + c;
 		
-		  
+		char value;
+		
+		int number;
+		
+		double data;
+		
+		boolean val1;
 		  
 		  
 		 
